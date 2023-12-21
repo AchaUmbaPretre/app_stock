67f6e89be1818c18605835230173cbc70b1d9ae7
AchaUmbaPretre <achandambi@gmail.com>
2023-12-21 16:36:23 +0100

    commit

--- src/pages/varianteProduit/VarianteProduit.jsx
+++ src/pages/varianteProduit/VarianteProduit.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import moment from 'moment';
 import { Image } from 'antd';
+import './varianteProduit.scss'
 import config from '../../config'
 
 const VarianteProduit = () => {
@@ -12,43 +13,9 @@ const VarianteProduit = () => {
     const [getProduit, setGetProduit] = useState([]);
     const [data, setData] = useState([]);
     const [getCouleur,setGetCouleur] = useState([]);
-    const [getPays,setGetPays] = useState([]);
     const navigate = useNavigate();
     const {pathname} = useLocation();
     const id = pathname.split('/')[2];
-    const [idCible,setIdcible] = useState();
-    const [idFamille, setIdFamille] = useState();
-
-
-    const handleInputChange = async (e) => {
-        const fieldName = e.target.name;
-        const fieldValue = e.target.value;
-      
-        let updatedValue = fieldValue;
-      
-        if (fieldName === "image") {
-          const file = e.target.files[0];
-          const reader = new FileReader();
-      
-          reader.onload = () => {
-            const base64File = reader.result;
-            updatedValue = base64File;
-            setData((prev) => ({ ...prev, [fieldName]: updatedValue }));
-          };
-      
-          reader.onerror = (error) => {
-            console.error("Erreur de lecture du fichier :", error);
-          };
-      
-          reader.readAsDataURL(file);
-        } else if (fieldName === "contact_email") {
-          updatedValue = fieldValue.toLowerCase();
-        } else if (Number.isNaN(Number(fieldValue))) {
-          updatedValue = fieldValue.charAt(0).toUpperCase() + fieldValue.slice(1);
-        }
-      
-        setData((prev) => ({ ...prev, [fieldName]: updatedValue }));
-      };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -62,13 +29,6 @@ const VarianteProduit = () => {
         };
         fetchData();
       }, [id]);
-
-      useEffect(()=>{
-        setIdcible(getProduit?.id_cible)
-        setIdFamille(getProduit?.id_famille)
-      },[getProduit?.id_cible])
-
-      console.log(idCible, idFamille)
 
       useEffect(() => {
         const fetchData = async () => {

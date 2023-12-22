4fd2b3935ff8c02d8f43874b7ba0d08a877409bd
AchaUmbaPretre <achandambi@gmail.com>
2023-12-22 13:31:00 +0100

    commit

---
+++ src/pages/PageDetails/PageDetails.jsx
@@ -0,0 +1,16 @@
+import './pageDetails.scss'
+
+const PageDetails = () => {
+  return (
+    <>
+        <div className="pageDetails">
+            <div className="pageDetail-wrapper">
+                
+            </div>
+        </div>
+
+    </>
+  )
+}
+
+export default PageDetails

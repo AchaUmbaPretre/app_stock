fc116eb94528749c257c0a435d0eeb4ccf88f0eb
AchaUmbaPretre <achandambi@gmail.com>
2023-12-22 12:46:15 +0100

    commit

--- src/pages/products/productView/ProductView.jsx
+++ src/pages/products/productView/ProductView.jsx
@@ -111,7 +111,7 @@ const ProductView = () => {
             confirmButtonText: 'OK',
           });
           
-          navigate('/products')
+          navigate('/varianteProduit')
           window.location.reload();
     
         }catch(err) {

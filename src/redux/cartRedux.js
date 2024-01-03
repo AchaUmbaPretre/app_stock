870bbc8860e4b923e574d00dc521937361db3ba6
AchaUmbaPretre <achandambi@gmail.com>
2024-01-03 13:28:35 +0100

    commit

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -1,36 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-  name: "cart",
-  initialState: {
-    products: [],
-    quantite: 0,
-    total:0,
-    totalPere: 0,
-  },
-  reducers: {
-    addProduct: (state, action) => {
+    name: "cart",
+    initialState: {
+      products: [],
+      quantite: 0,
+      total: 0,
+      totalPere: 0, // Nouvelle propriété pour le total du produit père
+    },
+    reducers: {
+      addProduct: (state, action) => {
         state.quantite += 1;
         state.products.push(action.payload);
         state.total += action.payload[0].prix * action.payload.quantite;
+  
+        const sousProduits = action.payload[0].prix; // Accéder au tableau des sous-produits du produit père
+            const quantiteProduitPere = action.payload.quantite; // Récupérer la quantité du produit père
 
-        console.log(action.payload.quantite)
-
-              // Calcul du total du produit père
-      const sousProduits = action.payload[0]; 
-      console.log('bonjour', sousProduits)
-      // Tableau des sous-produits du produit père
-      if (sousProduits) {
-        const totalSousProduits = sousProduits.reduce(
-          (total, sousProduit) =>
-            total + sousProduit.prix * sousProduit.quantite,
-          0
-        );
-        state.totalPere += totalSousProduits;
-      }
+            if (Array.isArray(sousProduits)) {
+            const totalSousProduits = sousProduits.reduce(
+                (total, sousProduit) =>
+                total + sousProduit.prix * sousProduit.quantite * quantiteProduitPere,
+                0
+            );
+            state.totalPere += totalSousProduits;
+            }
+                },
     },
-  },
-});
+  });
 
 export const { addProduct } = cartSlice.actions;
 export default cartSlice.reducer;

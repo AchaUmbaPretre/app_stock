4e06f179dc8fb48f4763519485e186a6eff86bad
AchaUmbaPretre <achandambi@gmail.com>
2024-01-03 13:07:20 +0100

    commit

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -6,12 +6,28 @@ const cartSlice = createSlice({
     products: [],
     quantite: 0,
     total:0,
+    totalPere: 0,
   },
   reducers: {
     addProduct: (state, action) => {
         state.quantite += 1;
         state.products.push(action.payload);
         state.total += action.payload[0].prix * action.payload.quantite;
+
+        console.log(action.payload.quantite)
+
+              // Calcul du total du produit père
+      const sousProduits = action.payload[0]; 
+      console.log('bonjour', sousProduits)
+      // Tableau des sous-produits du produit père
+      if (sousProduits) {
+        const totalSousProduits = sousProduits.reduce(
+          (total, sousProduit) =>
+            total + sousProduit.prix * sousProduit.quantite,
+          0
+        );
+        state.totalPere += totalSousProduits;
+      }
     },
   },
 });

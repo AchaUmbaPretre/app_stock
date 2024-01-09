0d9d2420ca8ed8ecd6cf4d3620e9e73ec7dded95
AchaUmbaPretre <achandambi@gmail.com>
2024-01-09 08:55:19 +0100

    commit

--- src/config.js
+++ src/config.js
@@ -3,12 +3,17 @@ import axios from "axios";
 /* const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken; */
+/* baseURL: 'http://localhost:8080' */
+/*       REACT_APP_SERVER_DOMAIN : 'http://localhost:8080' */
+/* REACT_APP_SERVER_DOMAIN : 'https://apindoe.loginsmart-cd.com' */
 
 export default {
+
       REACT_APP_SERVER_DOMAIN : 'http://localhost:8080'
+      
 };
 
 export const userRequest = axios.create({
       baseURL: 'http://localhost:8080',
       header: { token: `Bearer ${''}` },
-    });
+});

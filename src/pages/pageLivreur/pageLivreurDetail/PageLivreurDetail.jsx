81664af25825f0f9f9f9d132d5028bb48fc69909
AchaUmbaPretre <achandambi@gmail.com>
2024-01-10 12:25:04 +0100

    commit

--- src/pages/pageLivreur/pageLivreurDetail/PageLivreurDetail.jsx
+++ src/pages/pageLivreur/pageLivreurDetail/PageLivreurDetail.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './pageLivreurDetail.scss'
+import profil from '../../../assets/2e6501ef-e0e6-4968-af04-5a90fe0ffd69.jpg'
 
 const PageLivreurDetail = () => {
   return (
-    <div>
-        aaaaaaa
-    </div>
+    <>
+      <div className="pageLivreurDetail">
+        <div className="pageLivreurDetail-wrapper">
+          <img src={profil} alt="" className="img-page-livreur" />
+          <div className="pageLivreurDetail-rows">
+            <span className="pageLivreur-name">Produit1</span>
+            <span className="pageLivreur-name">Produit1</span>
+            <span className="pageLivreur-name">Produit1</span>
+            <span className="pageLivreur-name">Produit1</span>
+          </div>
+        </div>
+      </div>
+      
+    </>
   )
 }
 

236455eefe2481068fa304ea9a3a0d7df81a3a8b
AchaUmbaPretre <achandambi@gmail.com>
2024-02-16 09:06:45 +0100

    commit

--- src/components/rowTotal/RowTotal.jsx
+++ src/components/rowTotal/RowTotal.jsx
@@ -12,14 +12,15 @@ import axios from 'axios';
 const RowTotal = () => {
     const [venteTotal, setVenteTotal] = useState([]);
     const [produitTotalAchats, setProduitTotalAchats] = useState([]);
+    const [produitTotalDuel, setProduitTotalDuel] = useState([]);
     const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
           try {
-            const { data } = await axios.get(`${DOMAIN}/api/vente/venteTotal`);
-            setVenteTotal(data[0]?.vente_total);
+            const { data } = await axios.get(`${DOMAIN}/api/rapport/venteTotal/total`);
+            setVenteTotal(data[0]?.montant_total_vente);
             setLoading(false)
           } catch (error) {
             console.log(error);
@@ -31,8 +32,21 @@ const RowTotal = () => {
       useEffect(() => {
         const fetchData = async () => {
           try {
-            const { data } = await axios.get(`${DOMAIN}/api/produit/produitTotalAchats`);
-            setProduitTotalAchats(data[0]?.achats_total);
+            const { data } = await axios.get(`${DOMAIN}/api/rapport/achatsTotal/total`);
+            setProduitTotalAchats(data[0]?.montant_total_achats);
+            setLoading(false)
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAIN]);
+      
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/api/rapport/achatsTotalDuel/total`);
+            setProduitTotalDuel(data[0]?.montant_total_ventes_dues);
             setLoading(false)
           } catch (error) {
             console.log(error);
@@ -59,7 +73,7 @@ const RowTotal = () => {
                         <Money className='rowTotalIcon' style={{color: 'blue'}}/>
                     </div>
                     <div className="rowTotal-right">
-                        <h2><CountUp end={0}/>$</h2>
+                        <h2><CountUp end={produitTotalDuel}/>$</h2>
                         <span className="rowTotal-span">Ventes totales dues</span>
                     </div>
                 </div>

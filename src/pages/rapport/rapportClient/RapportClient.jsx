667e4c0496a5a547f268fbbe66e00f40686d504f
AchaUmbaPretre <achandambi@gmail.com>
2024-02-27 14:17:45 +0100

    commit

--- src/pages/rapport/rapportClient/RapportClient.jsx
+++ src/pages/rapport/rapportClient/RapportClient.jsx
@@ -231,7 +231,7 @@ item.nom_client.toLowerCase().includes(searchValue.toLowerCase())
                    {/* {open &&
                     <RapportVenteSelects getProduits={setGetRapport}/> } */}
                     <div className="rowChart-row-table">
-                        <Table columns={columns} dataSource={filteredData} loading={loading} scroll={scroll} pagination={{ pageSize: 5}} />
+                        <Table columns={columns} dataSource={filteredData} loading={loading} scroll={scroll} pagination={{ pageSize: 10}} />
                     </div>
                 </div>
             </div>
--- src/pages/rapport/rapportClient/RapportClientVenteOne.jsx
+++ src/pages/rapport/rapportClient/RapportClientVenteOne.jsx
@@ -271,7 +271,7 @@ item.nom_marque.toLowerCase().includes(searchValue.toLowerCase())
 {/*                     {open &&
                     <RapportVenteAllSelects getProduits={setGetRapport} id={id}/> } */}
                     <div className="rowChart-row-table">
-                        <Table columns={columns} dataSource={filteredData} loading={loading} scroll={scroll} pagination={{ pageSize: 5}} />
+                        <Table columns={columns} dataSource={filteredData} loading={loading} scroll={scroll} pagination={{ pageSize: 10}} />
                     </div>
                 </div>
             </div>

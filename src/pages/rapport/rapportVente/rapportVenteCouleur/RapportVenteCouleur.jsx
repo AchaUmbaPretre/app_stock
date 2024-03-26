93d381be7e4e4bebf54819181ada1ac2a2fecda7
AchaUmbaPretre <achandambi@gmail.com>
2024-03-26 15:45:42 +0100

    commit

--- src/pages/rapport/rapportVente/rapportVenteCouleur/RapportVenteCouleur.jsx
+++ src/pages/rapport/rapportVente/rapportVenteCouleur/RapportVenteCouleur.jsx
@@ -78,6 +78,16 @@ const columns = [
           </Popover>
         ),
       },
+      {
+        title: 'Nombre vendu',
+        dataIndex: 'nombre_vendu',
+        key: 'nombre_vendu',
+        sorter: (a, b) => a.nombre_vendu - b.nombre_vendu,
+        sortDirections: ['descend', 'ascend'],
+        render: (nombre_vendu) => (
+          <Tag color={nombre_vendu > 0 ? 'green' : 'red'}>{nombre_vendu}</Tag>
+        ),
+      },
      {
       title: 'Quantité vendue',
       dataIndex: 'quantite_vendue',

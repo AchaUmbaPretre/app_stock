62e43c412ab85e461a693f34981a5f2cd6c2d078
AchaUmbaPretre <achandambi@gmail.com>
2024-03-28 14:23:37 +0100

    commit

--- src/pages/livraison/livraison_detail/Livraison_detail.jsx
+++ src/pages/livraison/livraison_detail/Livraison_detail.jsx
@@ -41,13 +41,12 @@ const Livraison_detail = () => {
       const columns = [
         { title: '#', dataIndex: 'id', key: 'id', render: (text, record, index) => index + 1, width:"3%"},
         {
-          title: 'Commande N°',
+          title: 'Code',
           dataIndex: 'id_commande',
           key: 'id_commande',
-          render: (text) => 
-          <Tag color={'blue'}>
-            {text}
-          </Tag>
+          render: (text, record) => (
+            <Tag color="blue">{`${new Date().getFullYear().toString().substring(2)}${record.id_shop.toString().padStart(2, '0')}${record.id_commande.toString().padStart(2, '0')}`}</Tag>
+          ),
         },
         {
           title: 'Client',

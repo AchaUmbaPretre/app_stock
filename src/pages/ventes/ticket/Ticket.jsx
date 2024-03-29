fb5d023c2e7f91920e1d5f977264a268d2ef5cdc
AchaUmbaPretre <achandambi@gmail.com>
2024-03-29 09:33:52 +0100

    commit

--- src/pages/ventes/ticket/Ticket.jsx
+++ src/pages/ventes/ticket/Ticket.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import config from '../../../config';
 
 const Ticket = ({idTicket}) => {
+
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+    const [loading, setLoading] = useState(true);
     
-    
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/api/vente/${id}`);
+            setLoading(false)
+            setData(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAIN,id]);
+
   return (
     <div>Ticket</div>
   )

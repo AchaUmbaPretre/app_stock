7f3216e5355dc97cc4de885e5f283f675eab2b81
AchaUmbaPretre <achandambi@gmail.com>
2024-04-01 12:09:02 +0100

    commit

--- src/pages/dette/Dette.jsx
+++ src/pages/dette/Dette.jsx
@@ -41,7 +41,7 @@ const Dette = () => {
           dataIndex: 'id_commande',
           key: 'id_commande',
           render: (text, record) => (
-            <Tag color="blue">{`${new Date().getFullYear()?.toString().substring(2)}${record.id_shop?.toString().padStart(2, '0')}${record.id_commande.toString().padStart(2, '0')}`}</Tag>
+            <Tag color="blue">{`${new Date().getFullYear().toString().substring(2)}${record.id_shop.toString().padStart(2, '0')}${record.id_commande.toString().padStart(4, '0')}`}</Tag>
           ),
         },
         {

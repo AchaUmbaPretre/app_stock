d76e033f353d82b2fbc03459160f6d3ce24332d5
AchaUmbaPretre <achandambi@gmail.com>
2024-04-01 12:40:54 +0100

    commit

--- src/pages/dette/Dette.jsx
+++ src/pages/dette/Dette.jsx
@@ -167,7 +167,7 @@ const Dette = () => {
             render: (text, record) => (
                 
               <Space size="middle">
-                <Popover title="Voir la liste de dette de ce client" trigger="hover">
+                <Popover title={`Voir la liste de vente en crédit de Mme ${record.nom}`} trigger="hover">
                     <Link to={`/dette/${record.id_client}`}>
                       <Button icon={<EyeOutlined />} style={{ color: 'blue' }} />
                     </Link>

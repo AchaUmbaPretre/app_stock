676a3f62652d8c45c40acdb61e27a89b426ad874
AchaUmbaPretre <achandambi@gmail.com>
2024-04-03 16:42:42 +0100

    commit

--- src/pages/pageLivreur/localisation/Localisation.jsx
+++ src/pages/pageLivreur/localisation/Localisation.jsx
@@ -4,6 +4,7 @@ import { MapContainer, TileLayer, Popup } from 'react-leaflet';
 import { useLocation } from 'react-router-dom';
 import { EnvironmentOutlined,WhatsAppOutlined } from '@ant-design/icons';
 import L from 'leaflet';
+import { getDistance } from 'geolib';
 
 const Localisation = () => {
   const location = useLocation();
@@ -14,9 +15,33 @@ const Localisation = () => {
   const num = searchParams.get('num');
   const clientAddress = `Kinshasa, ${commune}, ${quartier}, ${num}`;
   const [currentPosition, setCurrentPosition] = useState(null);
+  const [boutiquePosition, setBoutiquePosition] = useState(null);
+  const [distance, setDistance] = useState(null)
+
+  const boutiqueCommune = 'Ngaliema';
+  const boutiqueQuartier = 'Lalu';
+  const boutiqueAvenue = 'kinshasa';
+  const boutiqueAddress = `Kinshasa, ${boutiqueCommune}, ${boutiqueQuartier}, ${boutiqueAvenue}`;
+
 
   useEffect(() => {
     const geocodeAddress = async () => {
+        if (boutiqueAddress) {
+            try {
+              const boutiqueResponse = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent('Kinshasa', 'ngaliema', 'lula','40')}`);
+              const boutiqueData = await boutiqueResponse.json();
+    
+              if (boutiqueData.length > 0) {
+                const { lat, lon } = boutiqueData[0];
+                setBoutiquePosition({ latitude: parseFloat(lat), longitude: parseFloat(lon) });
+              } else {
+                console.log('Aucun résultat de géocodage trouvé pour l\'adresse de la boutique spécifiée.');
+              }
+            } catch (error) {
+              console.error('Erreur lors du géocodage de l\'adresse de la boutique:', error);
+            }
+        }
+    
       if (clientAddress) {
         try {
           const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(clientAddress)}`);
@@ -35,7 +60,15 @@ const Localisation = () => {
     };
 
     geocodeAddress();
-  }, [clientAddress]);
+  }, [clientAddress,boutiqueAddress]);
+
+  useEffect(() => {
+    if (boutiquePosition && currentPosition) {
+      const calculatedDistance = getDistance(boutiquePosition, currentPosition);
+      setDistance(calculatedDistance);
+    }
+  }, [boutiquePosition, currentPosition]);
+
 
   const handleSendLocation = () => {
     // Ouvrir l'application WhatsApp avec la localisation pré-remplie dans le message
@@ -60,7 +93,9 @@ const Localisation = () => {
       {currentPosition && (
         <button onClick={handleSendLocation} style={{border:'none',padding:'10px', margin: '10px', borderRadius:'5px', display:'flex', alignItems:'center', gap:"5px",color: 'rgb(3, 3, 109)'}}>Partager la localisation en direct <WhatsAppOutlined style={{ fontSize: '15px', color: 'green' }}/></button>
       )}
+      <p style={{margin:'10px',color: 'rgb(3, 3, 109)'}}>Distance entre la boutique et le client : {distance && distance} mètres.</p>
     </div>
+    
   );
 };
 

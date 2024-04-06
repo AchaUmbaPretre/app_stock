d89102fcb9c47686ee5d41f18ab1942919812536
AchaUmbaPretre <achandambi@gmail.com>
2024-04-06 12:32:41 +0100

    commit

--- src/pages/rapport/rapportVente/rapportAjour/RapportAjour.jsx
+++ src/pages/rapport/rapportVente/rapportAjour/RapportAjour.jsx
@@ -54,34 +54,59 @@ const columns = [
       ),
     },
     {
-        title: 'Couleur',
-        dataIndex: 'description',
-        key: 'description',
-        render: (color) => {
-          let tagColor;
+      title: 'Couleur',
+      dataIndex: 'description',
+      key: 'description',
+      render: (color) => {
+        let tagColor;
       
-          if (color === 'Rouge') {
-            tagColor = 'red';
-          } else if (color === 'Noir') {
-            tagColor = 'black';
-          } else if (color === 'Orange') {
-            tagColor = 'orange';
-          } else if (color === 'Bleu') {
-            tagColor = 'skyblue';
-          } else if (color === 'Chocolat') {
-            tagColor = 'chocolate';
-          } else if (color === 'Vert fluo') {
-            tagColor = 'lime';
-          } else if (color === 'Rose fuchsia') {
-            tagColor = 'hotpink';
-          } else if (color === 'Beige saumon') {
-            tagColor = 'burlywood';
-          }
+        if (color === 'Rouge') {
+          tagColor = '#FF0000';
+        } else if (color === 'Noir') {
+          tagColor = 'black';
+        } else if (color === 'Noir brillant'){
+          tagColor = '#000';
+        } else if (color === 'Orange') {
+          tagColor = 'orange';
+        } else if (color === 'Bleu') {
+          tagColor = 'skyblue';
+        } else if (color === 'Bleu ciel'){
+          tagColor = '#87CEEB';
+        } else if (color === 'Chocolat') {
+          tagColor = 'chocolate';
+        } else if (color === 'Vert fluo') {
+          tagColor = 'lime';
+        } else if (color === 'Vert') {
+          tagColor = ' #008000';
+        }else if (color === 'Vert clair'){
+          tagColor = '#90EE90';
+        } else if (color === 'Rose fuschia') {
+          tagColor = '#FF00FF';
+        }else if (color === 'Rose'){
+          tagColor = '#FFC0CB';
+        } else if (color === 'Beige saumon') {
+          tagColor = 'burlywood';
+        } else if (color === 'Jaune') {
+          tagColor = 'yellow';
+        } else if (color === 'Gris') {
+          tagColor = 'gray';
+        } else if (color === 'Violet') {
+          tagColor = 'purple';
+        } else if (color === 'Mauve') {
+          tagColor = '#D473D4';
+        } else if (color === 'Argente'){
+          tagColor = '#C0C0C0';
+        } else if (color === 'Blanc'){
+          tagColor = 'white';
+        } else {
+          // Couleur par défaut si aucune correspondance n'est trouvée
+          tagColor = 'default';
+        }
       
-          return (
-            <Tag color={tagColor}>{color}</Tag>
-          );
-        },
+        return (
+          <Tag color={tagColor}>{color}</Tag>
+        );
+      },
     },
     {
         title: 'Date',

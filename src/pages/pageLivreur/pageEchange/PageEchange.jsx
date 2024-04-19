c81d1c41ff82c3b16428bd9783445fd7e035f608
AchaUmbaPretre <achandambi@gmail.com>
2024-04-19 16:51:10 +0100

    commit

--- src/pages/pageLivreur/pageEchange/PageEchange.jsx
+++ src/pages/pageLivreur/pageEchange/PageEchange.jsx
@@ -12,12 +12,14 @@ import { FadeLoader } from 'react-spinners';
 const PageEchange = () => {
     const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
     const [selected, setSelected] = useState([]);
+    const [selecteds, setSelecteds] = useState([]);
     const scroll = { x: 400 };
     const [isLoading, setIsLoading] = useState(false);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
     const [data, setData] = useState([]);
     const [desc, setDesc] = useState(null);
+    const [getCommandes, setGetCommandes] = ([]);
     const userId = useSelector((state) => state.user.currentUser.id);
     const {pathname} = useLocation();
     const IdCommande = pathname.split('/')[2];
@@ -27,6 +29,15 @@ const PageEchange = () => {
           setSelected([...selected, { id,id_commande,id_detail_commande,id_detail_livraison,qte_livre,prix,id_taille,id_client}]);
         } else {
           setSelected(selected.filter((row) => row.id !== id));
+        }
+      };
+
+      const handleSelectionChanges = (event,id_vente, id_commande,id_detail_commande,prix_unitaire,id_taille,id_client, id_varianteProduit) => {
+        if (event.target.checked) {
+          const updatedSelected = [...selected, { id_vente,id_commande,id_detail_commande,prix_unitaire,id_taille,id_client,id_varianteProduit}]
+          setSelecteds(updatedSelected);
+        } else {
+          setSelecteds(selected.filter((row) => row.id_detail_commande !== id_detail_commande));
         }
       };
 
@@ -86,6 +97,83 @@ const PageEchange = () => {
           }
       ];
 
+      const columnsEchange = [
+        {
+          title: '',
+          dataIndex: 'id_detail',
+          key: 'selected',
+          render: (text, record) => (
+            <div>
+              <Checkbox
+                checked={selected.some((item) => item.id_detail_commande === record.id_detail_commande)}
+                onChange={(event) =>
+                  handleSelectionChanges(event,record.id_vente,record.id_commande, record.id_detail_commande, record.prix_unitaire,record.id_taille,record.id_client,record.id_varianteProduit)
+                }
+              />
+            </div>
+          ),
+        },
+        { title: '#', dataIndex: 'id', key: 'id', render: (text, record, index) => index + 1, width:"2%"},
+          {
+            title: 'image',
+            dataIndex: 'img',
+            key: 'image',
+            width: '20px',
+            render: (text, record) => (
+              <div className="userList" onClick={()=>navigate(`/pageLivreurDetail/${record.id_varianteProduit}`)}>
+                <img src={`${DOMAIN}${record.img}`} alt="" className="userImg"  />
+              </div>
+            )
+        },
+        {
+          title: 'Pointure',
+          dataIndex: 'pointure',
+          key: 'pointure',
+          render: (text) => (
+            <Space>
+              <Tag color="green">{text}</Tag>
+            </Space>
+          ),
+        },
+        {
+          title: 'Prix',
+          dataIndex: 'prix_unitaire',
+          key: 'prix_unitaire',
+          sorter: (a, b) => a.prix_unitaire - b.prix_unitaire,
+          sortDirections: ['descend', 'ascend'],
+          render: (text) => (
+            <span>
+            <Tag color={'green'}>
+              {parseFloat(text).toLocaleString('fr-FR', {
+                style: 'currency',
+                currency: 'USD',
+              })}
+            </Tag>   
+            </span>
+          ),
+        },
+        {
+          title: 'Couleur',
+          dataIndex: 'description',
+          key: 'description',
+          render: (description) => (
+            <Space>
+              <Tag color="green">{description}</Tag>
+            </Space>
+          ),
+        },
+        {
+          title: 'Qté',
+          dataIndex: 'quantite',
+          key: 'quantite',
+          render: (text) => (
+            <Space>
+              <Tag color="green">{text}</Tag>
+            </Space>
+          ),
+        }
+      ];
+
       useEffect(() => {
         const fetchData = async () => {
           try {
@@ -109,6 +197,22 @@ const PageEchange = () => {
       
         fetchData();
       }, [DOMAIN, userId, IdCommande]);
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/api/produit/echanges/${IdCommande}`);
+            console.log(data)
+            setGetCommandes(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAIN,IdCommande]);
+
+      console.log(getCommandes)
+
 
       const handleClick = async (e) => {
         e.preventDefault();
@@ -176,8 +280,8 @@ const PageEchange = () => {
                   </div>
               </div>
                 <div className="rowChart-row-table">
-                        <h1>Sélectionnez le produit à échanger</h1>
-                    <Table columns={columns} dataSource={data} loading={loading} scroll={scroll} pagination={{ pageSize: 8}} />
+                    <h1>Sélectionnez le produit à échanger</h1>
+                    <Table columns={columnsEchange} dataSource={getCommandes} loading={loading} scroll={scroll} pagination={{ pageSize: 8}} />
                 </div>
                 <div className="rowChart-row-table">
                     <Table columns={columns} dataSource={data} loading={loading} scroll={scroll} pagination={{ pageSize: 8}} />

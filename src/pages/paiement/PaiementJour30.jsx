a0f5279927e1d71f9b531017eb7357d902325339
AchaUmbaPretre <achandambi@gmail.com>
2024-04-24 10:32:20 +0100

    commit

--- src/pages/paiement/PaiementJour30.jsx
+++ src/pages/paiement/PaiementJour30.jsx
@@ -137,7 +137,6 @@ const PaiementJour30 = () => {
                 <div className="product-bottom">
                       <div className="product-bottom-top">
                           <div className="product-bottom-left">
-                          {opens ?<CloseOutlined className='product-icon2' onClick={HandOpen} /> : <SisternodeOutlined className='product-icon' onClick={HandOpen} />}
                               <div className="product-row-search">
                                   <SearchOutlined className='product-icon-plus'/>
                                   <input type="search" name="" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder='Recherche...' className='product-search' />

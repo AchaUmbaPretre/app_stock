33deaa12a9591c17a6eedc1fa41d4bafb95c2336
AchaUmbaPretre <achandambi@gmail.com>
2024-05-01 12:14:16 +0100

    commit

--- src/pages/dette/DetteJours30.jsx
+++ src/pages/dette/DetteJours30.jsx
@@ -219,7 +219,7 @@ const DetteJours30 = () => {
             <div className="product-container">
                 <div className="product-container-top">
                         <div className="product-left">
-                            <h2 className="product-h2" style={{fontSize: '22px'}}>Ventes à crédit du jour</h2>
+                            <h2 className="product-h2" style={{fontSize: '22px'}}>Ventes à crédit des 7 derniers jours</h2>
                             <span></span>
                         </div>
                         <div className="" style={{background: '#fafafa', padding: "10px 15px", borderRadius: '10px', boxShadow: '0px 0px 15px -10px rgba(0,0,0,0.75)'}}>

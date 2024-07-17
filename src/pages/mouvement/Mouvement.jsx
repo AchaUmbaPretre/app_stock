39d1bfe62f15233d6e112f7cde545d4a53ffb292
AchaUmbaPretre <achandambi@gmail.com>
2024-07-17 16:07:47 +0100

    commit

--- src/pages/mouvement/Mouvement.jsx
+++ src/pages/mouvement/Mouvement.jsx
@@ -254,13 +254,13 @@ const Mouvement = () => {
       }
 
    const filteredData = data?.filter((item) =>
-    item.type_mouvement?.toLowerCase().includes(searchValue.toLowerCase()) ||
-    item.nom_client?.toLowerCase().includes(searchValue.toLowerCase()) ||
-    item.nom_marque?.toLowerCase().includes(searchValue.toLowerCase()) ||
-    item.nom_commune?.toLowerCase().includes(searchValue.toLowerCase()) || 
-    item.livreur?.toLowerCase().includes(searchValue.toLowerCase())
+      item.type_mouvement?.toLowerCase().includes(searchValue.toLowerCase()) ||
+      item.nom_client?.toLowerCase().includes(searchValue.toLowerCase()) ||
+      item.nom_marque?.toLowerCase().includes(searchValue.toLowerCase()) ||
+      item.nom_commune?.toLowerCase().includes(searchValue.toLowerCase()) || 
+      item.livreur?.toLowerCase().includes(searchValue.toLowerCase())
     )
-  
+
     return (
     <>
         <div className="products">

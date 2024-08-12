18a84fc85a2ed0cde8f94c8629a3233db2868057
AchaUmbaPretre <achandambi@gmail.com>
2024-08-12 11:57:44 +0100

    commit

--- src/pages/depenses/Depenses.jsx
+++ src/pages/depenses/Depenses.jsx
@@ -1,22 +1,18 @@
 import './../products/products.scss'
-import { SearchOutlined, SisternodeOutlined,DollarOutlined, PlusOutlined,UserOutlined, FilePdfOutlined, FileExcelOutlined,CalendarOutlined, PrinterOutlined, DeleteOutlined} from '@ant-design/icons';
+import { SisternodeOutlined,DollarOutlined, PlusOutlined,UserOutlined,CalendarOutlined, DeleteOutlined} from '@ant-design/icons';
 import React, { useEffect, useState } from 'react';
-import { Button, Space, Table, Popover,Popconfirm, Tag, Modal} from 'antd';
+import { Button, Space, Table, Popover,Popconfirm, Tag, Modal, Input} from 'antd';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import FormDepenses from './FormDepenses';
 import config from '../../config';
 import moment from 'moment';
-import { useLocation } from 'react-router-dom';
 
 const Depenses = ({dateId}) => {
     const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
     const [searchValue, setSearchValue] = useState('');
-/*     const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const dateId = searchParams.get('date'); */
     const scroll = { x: 400 };
     const [open, setOpen] = useState(false);
     const user = useSelector((state) => state.user?.currentUser);
@@ -144,8 +140,6 @@ const Depenses = ({dateId}) => {
       setOpen(true)
     };
 
-    console.log(data)
-
   const filteredData = data?.filter((item) =>
   item.nom_categorie?.toLowerCase().includes(searchValue.toLowerCase())
 );
@@ -168,15 +162,15 @@ const Depenses = ({dateId}) => {
                     <div className="product-bottom-top">
                         <div className="product-bottom-left">
                             <SisternodeOutlined className='product-icon' />
-                            <div className="product-row-search">
-                                <SearchOutlined className='product-icon-plus'/>
-                                <input type="search" name="" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder='Recherche...' className='product-search' />
-                            </div>
+                            <Input
+                              type="search"
+                              value={searchValue}
+                              onChange={(e) => setSearchValue(e.target.value)}
+                              placeholder="Recherche..."
+                              className="product-search"
+                            />
                         </div>
                         <div className="product-bottom-right">
-                            <FilePdfOutlined className='product-icon-pdf' />
-                            <FileExcelOutlined className='product-icon-excel'/>
-                            <PrinterOutlined className='product-icon-printer'/>
                         </div>
                     </div>
                     <div className="rowChart-row-table">
@@ -191,7 +185,7 @@ const Depenses = ({dateId}) => {
                           footer={[
                           ]}
                         >
-                            <FormDepenses/>
+                          <FormDepenses/>
                         </Modal>
                         
                         <Table columns={columns} dataSource={filteredData} loading={loading} scroll={scroll} pagination={{ pageSize: 10}} />

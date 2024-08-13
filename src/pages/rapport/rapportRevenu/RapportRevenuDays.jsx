a2c963fc1935c02fd405c9aebfe21eb062215f06
AchaUmbaPretre <achandambi@gmail.com>
2024-08-13 13:32:25 +0100

    commit

--- src/pages/rapport/rapportRevenu/RapportRevenuDays.jsx
+++ src/pages/rapport/rapportRevenu/RapportRevenuDays.jsx
@@ -1,13 +1,11 @@
 import { SearchOutlined, CloseOutlined,SisternodeOutlined, ArrowUpOutlined, ArrowDownOutlined,DollarOutlined } from '@ant-design/icons';
-import { Input, Modal, Popover, Table, Tag } from 'antd';
+import { Input, Table, Tag } from 'antd';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import config from '../../../config';
 import RapportRevenuSelect from './RapportRevenuSelect';
-import CountUp from 'react-countup';
-import RapportRevenuDays from './RapportRevenuDays';
 
-const RapportRevenu = () => {
+const RapportRevenuDays = ({month}) => {
     const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
     const [getRapport, setGetRapport] = useState([]);
     const [getRap, setGetRap] = useState([]);
@@ -15,12 +13,6 @@ const RapportRevenu = () => {
     const [searchValue, setSearchValue] = useState('');
     const scroll = { x: 400 };
     const [open, setOpen] = useState(false);
-    const [month, setMonth] = useState('');
-
-    const HandOpen = (e) =>{
-      setOpen(!open)
-      setMonth(e)
-    }
     
 const columns = [
     { title: '#', dataIndex: 'id', key: 'id', render: (text, record, index) => index + 1 },
@@ -29,9 +21,7 @@ const columns = [
       dataIndex: 'mois',
       key: 'mois',
       render: (mois) => (
-        <Popover title={`Voir le detail de ${mois}`} trigger="hover">
-          <Tag color={'blue'} onClick={()=>HandOpen(mois) }>{mois}</Tag>
-        </Popover>      
+        <Tag color={'blue'}>{mois}</Tag>
       ),
     },
     {
@@ -88,11 +78,14 @@ const columns = [
 }
 ];
 
+const HandOpen = () =>{
+  setOpen(!open)
+}
 
 useEffect(() => {
   const fetchData = async () => {
     try {
-      const { data } = await axios.get(`${DOMAIN}/api/rapport/rapportRevenu/revenu`);
+      const { data } = await axios.get(`${DOMAIN}/api/rapport/rapportRevenu/revenuAllDay?months=${month}`);
       setGetRapport(data);
       setLoading(false)
     } catch (error) {
@@ -100,46 +93,16 @@ useEffect(() => {
     }
   };
   fetchData();
-}, [DOMAIN]);
-
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const { data } = await axios.get(`${DOMAIN}/api/rapport/rapportRevenu/revenuRapDuMois`);
-      setGetRap(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  fetchData();
-}, [DOMAIN]);
+}, [DOMAIN,month]);
 
 const filteredData = getRapport?.filter((item) =>
 item.mois.toLowerCase().includes(searchValue.toLowerCase())
 )
 
-console.log("Filtered Data:", filteredData);
-
   return (
     <>
         <div className="products">
             <div className="product-container">
-                <div className="product-container-top">
-                    <div className="product-left">
-                        <h2 className="product-h2">Rapport des revenus</h2>
-                        <span>Gérez votre revenus</span>
-                    </div>
-                    <div className="" style={{background: '#fafafa', padding: "10px 15px", borderRadius: '10px', boxShadow: '0px 0px 15px -10px rgba(0,0,0,0.75)'}}>
-                      <div style={{ display: 'flex', fontSize: '12px', marginBottom:'8px', fontWeight: 'bold' }}>
-                        {`Mois : ${getRap[0]?.mois}`}
-                      </div>
-                      <div style={{ display: 'flex', flexDirection: 'column',gap: '6px', fontSize: '13px' }}>
-                        <p style={{display:'flex',gap:'5px', justifyContent: 'space-between'}}>Nbre d'article vendue: <b style={{color:'#fff', background:'rgba(1, 35, 138, 0.952)', padding: "5px", borderRadius: '10px', fontSize: '12px'}}><CountUp end={getRap[0]?.quantite_vendue}/></b></p>
-                        <p style={{display:'flex',gap:'5px', justifyContent: 'space-between'}}>Nbre de commande: <b style={{color:'#fff', background:'rgba(1, 35, 138, 0.952)', padding: "5px", borderRadius: '10px', fontSize: '12px'}}><CountUp end={getRap[0]?.nombre_vente}/></b></p>
-                        <p style={{display:'flex',gap:'5px', justifyContent: 'space-between'}}>Revenu total: <b style={{color:'#fff', background:'rgba(1, 35, 138, 0.952)', padding: "5px", borderRadius: '10px', fontSize: '12px'}}><CountUp end={getRap[0]?.revenu_total}/></b></p>
-                      </div>
-                    </div>
-                </div>
                 <div className="product-bottom">
                     <div className="product-bottom-top">
                         <div className="product-bottom-left">
@@ -161,22 +124,11 @@ console.log("Filtered Data:", filteredData);
                     <div className="rowChart-row-table">
                         <Table columns={columns} dataSource={filteredData} loading={loading} scroll={scroll} pagination={{ pageSize: 12}} />
                     </div>
-                    <Modal
-                      title="Information du client"
-                      centered
-                      open={open}
-                      onCancel={() => setOpen(false)}
-                      width={1150}
-                      footer={[]}
-                    >
-                      <RapportRevenuDays month={month}/>
-                    </Modal>
                 </div>
             </div>
         </div>
-
     </>
   )
 }
 
-export default RapportRevenu
+export default RapportRevenuDays
